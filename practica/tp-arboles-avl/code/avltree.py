23bb95984ed46b34987b11ac353d4a558b66c7ac
Lautaro Larosa <lautarolarosacn56@gmail.com>
2023-03-13 00:00:35 -0300

    Rotación a la izquierda, a la derecha y el balanceFactor

--- practica/tp-arboles-avl/code/avltree.py
+++ practica/tp-arboles-avl/code/avltree.py
@@ -13,3 +13,88 @@ class AVLNode:
     bf = None
 
 
+#rotateLeft(Tree,avlnode) 
+#Descripción: Implementa la operación rotación a la izquierda 
+#Entrada: Un Tree junto a un AVLnode sobre el cual se va a operar la rotación a la  izquierda
+#Salida: retorna la nueva raíz
+
+def rotateLeft(Tree,avlnode):
+      newroot = avlnode.rightnode
+      newroot.parent = avlnode.parent
+      if avlnode.parent == None:
+            Tree.root = newroot
+      else:
+            if avlnode.parent.leftnode == avlnode:
+                  avlnode.parent.leftnode = newroot
+            else:
+                  avlnode.parent.rightnode = newroot
+      
+      avlnode.rightnode = newroot.leftnode
+      avlnode.parent = newroot
+      newroot.leftnode = avlnode
+      if avlnode.rightnode != None:
+            avlnode.rightnode.parent = avlnode
+
+
+
+
+#rotateRight(Tree,avlnode) 
+#Descripción: Implementa la operación rotación a la derecha 
+#Entrada: Un Tree junto a un AVLnode sobre el cual se va a operar la rotación a la  derecha
+#Salida: retorna la nueva raíz
+
+def rotateRight(Tree,avlnode):
+      newroot = avlnode.leftnode
+      newroot.parent = avlnode.parent
+      if avlnode.parent == None:
+            Tree.root = newroot
+      else:
+            if avlnode.parent.leftnode == avlnode:
+                  avlnode.parent.leftnode = newroot
+            else:
+                  avlnode.parent.rightnode = newroot
+
+      avlnode.leftnode = newroot.rightnode
+      avlnode.parent = newroot
+      newroot.rightnode = avlnode
+      if avlnode.leftnode != None:
+            avlnode.leftnode.parent = avlnode
+
+
+
+
+#calculateBalance(AVLTree) 
+#Descripción: Calcula el factor de balanceo de un árbol binario de búsqueda. 
+#Entrada: El árbol AVL  sobre el cual se quiere operar.
+#Salida: El árbol AVL con el valor de balanceFactor para cada subarbol
+
+def calculateBalance(AVLTree):
+      if AVLTree.root != None:
+            if AVLTree.root.leftnode == None and AVLTree.root.rightnode == None:
+                  AVLTree.root.bf = 0
+                  return 
+            else:
+                  calculateBalanceR(AVLTree.root)
+                  return 
+      else:
+            return
+
+
+# Función Recursiva que calcula el balanceo
+
+def calculateBalanceR(currentnode):
+      #Caso Base
+      if currentnode == None:
+            return 0
+
+      #Caso general
+      leftH = calculateBalanceR(currentnode.leftnode)
+      rightH = calculateBalanceR(currentnode.rightnode)
+
+      currentnode.bf = leftH - rightH
+
+      if leftH >= rightH:
+            return leftH + 1
+      else:
+            return rightH + 1
+
--- practica/tp-arboles-avl/code/main.py
+++ practica/tp-arboles-avl/code/main.py
@@ -1,9 +1,36 @@
 from avltree import *
-from binarytree import *
 
-newtree=BinaryTree()
+tree = AVLTree()
+nodo1 = AVLNode()
+nodo2 = AVLNode()
+nodo3 = AVLNode()
+nodo4 = AVLNode()
+nodo5 = AVLNode()
+nodo6 = AVLNode()
 
-insert(newtree,15,5)
-print(newtree.root.key)
+
+tree.root = nodo5
+
+nodo1.value = "A"
+nodo2.value = "B"
+nodo3.value = "C"
+nodo4.value = "D"
+nodo5.value = "E"
+nodo6.value = "F"
+
+nodo5.rightnode = nodo6
+nodo6.parent = nodo5
+nodo5.leftnode = nodo3
+nodo3.parent = nodo5
+nodo3.rightnode = nodo4
+nodo4.parent = nodo3
+nodo3.leftnode = nodo2
+nodo2.parent = nodo3
+nodo2.leftnode = nodo1
+nodo1.parent = nodo2
 
 
+calculateBalance(tree)
+
+print(" ")
+print(tree.root.bf)
