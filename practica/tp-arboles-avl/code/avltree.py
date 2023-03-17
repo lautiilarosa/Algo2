62df1c7c118f64ea91ca921cb96b7b115fab694f
Lautaro Larosa <lautarolarosacn56@gmail.com>
2023-03-17 15:04:23 -0300

    Rebalance y Parte 2

--- practica/tp-arboles-avl/code/avltree.py
+++ practica/tp-arboles-avl/code/avltree.py
@@ -74,22 +74,22 @@ def calculateBalance(AVLTree):
                   AVLTree.root.bf = 0
                   return 
             else:
-                  calculateBalanceR(AVLTree.root)
+                  update_bf(AVLTree.root)
                   return 
       else:
             return
 
 
-# Función Recursiva que calcula el balanceo
+# Función Recursiva que calcula el balanceo de un nodo raíz
 
-def calculateBalanceR(currentnode):
+def update_bf(currentnode):
       #Caso Base
       if currentnode == None:
             return 0
 
       #Caso general
-      leftH = calculateBalanceR(currentnode.leftnode)
-      rightH = calculateBalanceR(currentnode.rightnode)
+      leftH = update_bf(currentnode.leftnode)
+      rightH = update_bf(currentnode.rightnode)
 
       currentnode.bf = leftH - rightH
 
@@ -97,4 +97,42 @@ def calculateBalanceR(currentnode):
             return leftH + 1
       else:
             return rightH + 1
+
+
+#reBalance(AVLTree) 
+#Descripción: balancea un árbol binario de búsqueda. Para esto se deberá primero calcular el balanceFactor del árbol y luego en función de esto aplicar la estrategia de rotación que corresponda.
+#Entrada: El árbol binario de tipo AVL  sobre el cual se quiere operar.
+#Salida: Un árbol binario de búsqueda balanceado. Es decir luego de esta operación se cumple que la altura (h) de su subárbol derecho e izquierdo difieren a lo sumo en una unidad.
+
+def reBalance(AVLTree):
+      calculateBalance(AVLTree)
+      rebalanceR(AVLTree.root,AVLTree)
+
+# Un rebalance Recursivo en donde analizo el balancefactor de los nodos más profundos y realizo las rotaciones adecuadas
+# Calculo el balance factor del árbol después de una rotación, ya que una vez balanceado un subárbol que no estaba balanceado, el árbol vuelve a estar balanceado
+def rebalanceR(currentnode,AVLTree):
+      #Caso Base
+      if currentnode == None:
+            return
+
+      #Caso general
+      rebalanceR(currentnode.leftnode,AVLTree)
+      rebalanceR(currentnode.rightnode,AVLTree)
+
+      if currentnode.bf < -1:
+            if currentnode.rightnode.bf > 0:
+                  rotateRight(AVLTree,currentnode.rightnode)
+                  rotateLeft(AVLTree,currentnode)
+            else:
+                  rotateLeft(AVLTree,currentnode)
+      elif currentnode.bf > 1:
+            if currentnode.leftnode.bf < 0:
+                  rotateLeft(AVLTree,currentnode.leftnode)
+                  rotateRight(AVLTree,currentnode.rightnode)
+            else:
+                  rotateRight(AVLTree,currentnode)
+      else:
+            return
+
+      calculateBalance(AVLTree)            
 
--- practica/tp-arboles-avl/code/main.py
+++ practica/tp-arboles-avl/code/main.py
@@ -6,31 +6,29 @@ nodo2 = AVLNode()
 nodo3 = AVLNode()
 nodo4 = AVLNode()
 nodo5 = AVLNode()
-nodo6 = AVLNode()
 
 
-tree.root = nodo5
+tree.root = nodo1
+
+nodo1.leftnode = nodo2
+nodo2.parent = nodo1
+nodo1.rightnode = nodo3
+nodo3.parent = nodo1
+nodo2.rightnode = nodo4
+nodo4.parent = nodo2
+nodo4.leftnode = nodo5
+nodo5.parent = nodo4
 
 nodo1.value = "A"
 nodo2.value = "B"
 nodo3.value = "C"
 nodo4.value = "D"
 nodo5.value = "E"
-nodo6.value = "F"
 
-nodo5.rightnode = nodo6
-nodo6.parent = nodo5
-nodo5.leftnode = nodo3
-nodo3.parent = nodo5
-nodo3.rightnode = nodo4
-nodo4.parent = nodo3
-nodo3.leftnode = nodo2
-nodo2.parent = nodo3
-nodo2.leftnode = nodo1
-nodo1.parent = nodo2
+print("Se está por balancear el árbol")
+reBalance(tree)
+print("")
+print("Se acaba de balancear el árbol")
 
+print(tree.root.leftnode.value)
 
-calculateBalance(tree)
-
-print(" ")
-print(tree.root.bf)
